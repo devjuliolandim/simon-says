8bf98db56af631cf900ce065a09bdd28e4cb65ff
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-02-19 09:35:54 -0300

    favicon

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-//Constants
+//Constants DOM
+const GREEN = document.getElementById('green')
+const RED = document.getElementById('red')
+const YELLOW = document.getElementById('yellow')
+const BLUE = document.getElementById('blue')
+const PLAY_BUTTON = document.getElementById('play')
+
+
+//Color Constants
+const LIGHT_RED = "rgb(255, 150, 150)"
+
+
+
+//Functions
+PLAY_BUTTON.addEventListener('click', () => {
+
+
+})
+
