fdee58a6ac7e4e305d625d7da8f23801372d6795
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-02-19 12:04:22 -0300

    color changing correctly

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,7 @@ const LIGHT_YELLOW = "lightyellow"
 
 //Variables
 let game_started = false
+let random = 0
 
 //EventListener
 PLAY_BUTTON.addEventListener('click', playClick)
@@ -24,27 +25,23 @@ RESET_BUTTON.addEventListener('click', resetClick)
 //Functions
 function playClick(){
     
+    PLAY_BUTTON.removeEventListener('click', playClick)
+
     game_started = true
     
     startVisualChanges()
 
-    let random = Math.floor(Math.random()* 4 + 1)
+    random = Math.floor(Math.random() * 4 + 1)
 
-    switch(random){
-        case 1:
-            GREEN_PAD.style.backgroundColor = LIGHT_GREEN;
-            break;
-        case 2:
-            RED_PAD.style.backgroundColor = LIGHT_RED;
-            break;
-        case 2:
-            YELLOW_PAD.style.backgroundColor = LIGHT_YELLOW;
-            break;
-        case 4:
-            BLUE_PAD.style.backgroundColor = LIGHT_BLUE;
-            break;    
+    if(random == 1){
+        changePadColor("green")
+    }else if(random == 2){
+        changePadColor("red")
+    }else if(random == 3){
+        changePadColor("yellow")
+    }else if(random == 4){
+        changePadColor("blue")
     }
-
 }
 
 
@@ -52,13 +49,15 @@ function resetClick(){
     if(game_started == false){
         alert("Hey, you have to click in the PLAY! button first.")
     }else{
+        PLAY_BUTTON.addEventListener('click', playClick)
+
         game_started = false
+
         resetVisualChanges()
     }
 }
 
 function startVisualChanges(){
-    PLAY_BUTTON.removeEventListener('click', playClick)
     PLAY_BUTTON.style.color = "black"
     PLAY_BUTTON.style.backgroundColor = "rgba(0, 0, 0, 0.047)"
     PLAY_BUTTON.classList.remove('hover')
@@ -69,7 +68,6 @@ function startVisualChanges(){
 }
 
 function resetVisualChanges(){
-    PLAY_BUTTON.addEventListener('click', playClick)
     PLAY_BUTTON.style.color = "white"
     PLAY_BUTTON.style.backgroundColor = "black"
     PLAY_BUTTON.classList.add('hover')
@@ -77,4 +75,43 @@ function resetVisualChanges(){
     RESET_BUTTON.style.backgroundColor = "rgba(0, 0, 0, 0.047)"
     RESET_BUTTON.style.color = "black"
     RESET_BUTTON.classList.remove('hover')
+
+    GREEN_PAD.style.backgroundColor = "green"
+    RED_PAD.style.backgroundColor = "red"
+    YELLOW_PAD.style.backgroundColor = "yellow"
+    BLUE_PAD.style.backgroundColor = "blue"
+}
+
+function changePadColor(color){
+
+    if(color == "green"){
+        GREEN_PAD.style.backgroundColor = LIGHT_GREEN
+
+        setTimeout(() =>{
+            GREEN_PAD.style.backgroundColor = "green"
+        }, 1000)
+
+    }else if(color == "red"){
+        RED_PAD.style.backgroundColor = LIGHT_RED
+
+        setTimeout(() =>{
+            RED_PAD.style.backgroundColor = "red"
+        }, 1000)
+
+    }else if(color == "yellow"){
+        YELLOW_PAD.style.backgroundColor = LIGHT_YELLOW
+
+        setTimeout(() =>{
+            YELLOW_PAD.style.backgroundColor = "yellow"
+        }, 1000)
+
+    }else if(color == "blue"){
+        BLUE_PAD.style.backgroundColor = "lightblue"
+
+        setTimeout(() => {
+            BLUE_PAD.style.backgroundColor = "blue"
+        }, 1000)
+    }
+
+
 }
